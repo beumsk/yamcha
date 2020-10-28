0eb4c8c19a33a37f42708a079c1e7a9dbe7586c8
Rémy Beumier <beumieremy@gmail.com>
2020-10-28 10:10:52 +0100

    files classed

---
+++ js/yamcha.js
@@ -0,0 +1,52 @@
+
+  // accordion
+  bulmaCollapsible.attach();
+  // accordion end
+
+  // carousel
+  bulmaCarousel.attach('#carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 3
+  });
+  bulmaCarousel.attach('#carousel-video', {
+    slidesToScroll: 1,
+    slidesToShow: 3
+  });
+  bulmaCarousel.attach('#carousel-card', {
+    slidesToScroll: 1,
+    slidesToShow: 3
+  });
+  // carousel end
+
+  // tabs script
+  function tabs(e) {
+    var siblings = e.currentTarget.parentNode.querySelectorAll("li");
+    for (var i=0; i<siblings.length; i++) {
+      siblings[i].classList.remove("is-active");
+    }
+    e.currentTarget.classList.add("is-active");
+    var index = Array.prototype.slice.call(siblings).indexOf(e.currentTarget);
+    var tabs = e.currentTarget.parentNode.parentNode.parentNode.querySelectorAll(".tab div");
+    for (var i=0; i<tabs.length; i++) {
+      tabs[i].classList.add("is-hidden");
+    }
+    tabs[index].classList.remove("is-hidden");
+  }
+  // tabs script end
+
+
+  // burger menu script
+  document.addEventListener('DOMContentLoaded', function() {
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if ($navbarBurgers.length > 0) {
+      $navbarBurgers.forEach( function(el) {
+        el.addEventListener('click', function() {
+          var target = el.dataset.target;
+          var $target = document.getElementById(target);
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        });
+      });
+    }
+  });
+  // burger menu script end

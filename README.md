9763c8549f3bbf906b249020e66c52d7ccfbef10
Rémy Beumier <beumieremy@gmail.com>
2023-04-03 22:03:50 +0200

    carousel gap + card no radius + modal script

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ Page layout built using Bulma.io (https://bulma.io)
 
 Using Bulma extensions from: https://bulma.io/extensions/
 
+- handle extensions with local js or dependency?
+
 ## To Do
+
 - review custom yamcha css
 - pick my own color palette
 - Add icons
-- Create subpages for each section
+- Create subpages for each section (how to handle pages? react or easier?)
 - Create different page layouts
-- Check uskinned
-- Improve script to take all component into account (multiple modal on a single page)
+- Check uskinned for inspiration of layouts and more
+- switch accordion (no js or change js)
--- js/yamcha.js
+++ js/yamcha.js
@@ -1,72 +1,96 @@
+// accordion
+bulmaCollapsible.attach();
+// accordion end
 
-  // accordion
-  bulmaCollapsible.attach();
-  // accordion end
+// carousel
+bulmaCarousel.attach('#carousel', {
+  slidesToScroll: 1,
+  slidesToShow: 3,
+});
+bulmaCarousel.attach('#carousel-video', {
+  slidesToScroll: 1,
+  slidesToShow: 3,
+});
+bulmaCarousel.attach('#carousel-card', {
+  slidesToScroll: 1,
+  slidesToShow: 3,
+});
+// carousel end
 
-
-  // carousel
-  bulmaCarousel.attach('#carousel', {
-    slidesToScroll: 1,
-    slidesToShow: 3
-  });
-  bulmaCarousel.attach('#carousel-video', {
-    slidesToScroll: 1,
-    slidesToShow: 3
-  });
-  bulmaCarousel.attach('#carousel-card', {
-    slidesToScroll: 1,
-    slidesToShow: 3
-  });
-  // carousel end
-
-
-  // tabs script
-  function tabs(e) {
-    var siblings = e.currentTarget.parentNode.querySelectorAll("li");
-    for (var i=0; i<siblings.length; i++) {
-      siblings[i].classList.remove("is-active");
-    }
-    e.currentTarget.classList.add("is-active");
-    var index = Array.prototype.slice.call(siblings).indexOf(e.currentTarget);
-    var tabs = e.currentTarget.parentNode.parentNode.parentNode.querySelectorAll(".tab div");
-    for (var i=0; i<tabs.length; i++) {
-      tabs[i].classList.add("is-hidden");
-    }
-    tabs[index].classList.remove("is-hidden");
+// tabs script
+function tabs(e) {
+  var siblings = e.currentTarget.parentNode.querySelectorAll('li');
+  for (var i = 0; i < siblings.length; i++) {
+    siblings[i].classList.remove('is-active');
   }
-  // tabs script end
+  e.currentTarget.classList.add('is-active');
+  var index = Array.prototype.slice.call(siblings).indexOf(e.currentTarget);
+  var tabs =
+    e.currentTarget.parentNode.parentNode.parentNode.querySelectorAll(
+      '.tab div'
+    );
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].classList.add('is-hidden');
+  }
+  tabs[index].classList.remove('is-hidden');
+}
+// tabs script end
 
-
-  // modal script
-  var modal = document.querySelector('.modal'); 
-  var modalBtn = document.querySelector('.modal-button');
-  var modalClose = document.querySelector('.modal-close');
-  var modalCancel = document.querySelector('.modal-cancel');
-
-  modalBtn.addEventListener('click', function () { modal.style.display = 'flex' });
-  modalClose.addEventListener('click', function () { modal.style.display = 'none' });
-  modalCancel.addEventListener('click', function () { modal.style.display = 'none' });
-
-  window.addEventListener('click', function (event) { 
-    if (event.target.className === 'modal-background') { 
-      modal.style.display = 'none' 
-    } 
+// modal script
+document.addEventListener('DOMContentLoaded', () => {
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
   });
-  // modal script
+  (
+    document.querySelectorAll(
+      '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button'
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest('.modal');
 
-
-  // burger menu script
-  document.addEventListener('DOMContentLoaded', function() {
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    if ($navbarBurgers.length > 0) {
-      $navbarBurgers.forEach( function(el) {
-        el.addEventListener('click', function() {
-          var target = el.dataset.target;
-          var $target = document.getElementById(target);
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-        });
-      });
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+    if (e.key === 'Escape') {
+      closeAllModals();
     }
   });
-  // burger menu script end
+});
+// modal script
+
+// burger menu script
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
+  );
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function (el) {
+      el.addEventListener('click', function () {
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
+// burger menu script end
